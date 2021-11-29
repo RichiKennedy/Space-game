8651e4a65f0752980273ac97bffb7816764f33fe
GolnooshMasoudi <90833819+GolnooshMasoudi@users.noreply.github.com>
2021-11-29 16:40:58 +0100

    change health status, add overlap

---
+++ script/main.js
@@ -0,0 +1,19 @@
+import Phaser, { AUTO } from "phaser";
+import GameScene from "./GameScene";
+
+const config = {
+    type : Phaser.AUTO,
+    width: 800,
+    height: 600,
+physics:{
+    default:"arcade",
+    arcade:{
+        debug:true,
+        gravity: false,
+    },
+},
+scene :[GameScene],
+};
+
+var game = new Phaser.Game(config);
+
